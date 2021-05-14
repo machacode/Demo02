596a6366eee7187e9c29e1a31bb7e6288c22d857
unknown <cmacha@outlook.com>
2021-05-13 22:42:30 -0500

    Segundo demo en vs.net

---
+++ Demo01.AppWebMvc/Controllers/ConsultaController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Demo01.AppWebMvc.Controllers
+{
+    public class ConsultaController : Controller
+    {
+        public ActionResult Inicio()
+        {
+            return View();
+        }
+
+        public string listarUbigeo()
+        {
+            var url = $"https://api.samishop.pe/v1/store/971/ubigeo";
+            string responseBody ="";
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            request.ContentType = "application/json";
+            request.Accept = "application/json";
+            try
+            {
+                using (WebResponse response = request.GetResponse())
+                {
+                    using (Stream strReader = response.GetResponseStream())
+                    {
+                        if (strReader != null)
+                        {
+                            using (StreamReader objReader = new StreamReader(strReader))
+                            {
+                                responseBody = objReader.ReadToEnd();
+                                
+                                // Do something with responseBody
+                                //Console.WriteLine(responseBody);
+                            }
+                        }
+                        
+                    }
+                }
+            }
+            catch (WebException ex)
+            {
+                // Handle error
+            }
+            return responseBody;
+        }
+
+    }
+}
---
+++ Demo01.AppWebMvc/Scritps/Consulta.js
@@ -0,0 +1,70 @@
+﻿var horaInicio;
+
+window.onload = function () {
+    horaInicio = new Date();
+    get("Consulta/listarUbigeo", mostrarPaises);
+}
+
+function get(url, callback) {
+    requestServer("get", url, callback);
+}
+
+function requestServer(metodoHttp, url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    xhr.open(metodoHttp, url);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            callback(xhr.response);
+        }
+    }
+    xhr.send();
+}
+
+function mostrarPaises(rpta) {
+    console.log(rpta);
+
+    if (rpta != null) {
+        crearTablaJson(rpta, "divTabla", "spnTotalReg")
+    }
+
+    var horaFin = new Date();
+    var tiempo = horaFin - horaInicio;
+    spnTiempo.innerHTML = tiempo + "msg";
+}
+
+function crearTablaJson(json, nombreDiv, spnTotal) {
+    var campos = Object.keys(json[0]);
+    var nRegistros = json.length;
+    var fila;
+    var nCampos = campos.length;
+    var col;
+    var checkHtml = "";
+    var comboHtml = "";
+    var html = "<table><thead><tr class='FilaCabecera'>";
+    for (var j = 0; j < nCampos; j++) {
+        col = campos[j];
+        html += "<th>";
+        html += col;
+        html += "</th>";
+    }
+    html += "</tr></thead><tbody>";
+    comboHtml += "<option value=''>Seleccionar</option>";
+    for (var i = 0; i < nRegistros; i++) {
+        fila = json[i];
+        html += "<tr class='FilaDatos'>";
+        for (var j = 0; j < nCampos; j++) {
+            col = fila[campos[j]];
+            html += "<td>";
+            html += col;
+            html += "</td>";
+        }
+        html += "</tr>";
+    }
+    html += "</tbody></table><br><br>";
+
+    var div = document.getElementById(nombreDiv);
+    div.innerHTML = html + checkHtml;
+    var span = document.getElementById(spnTotal);
+    span.innerHTML = nRegistros;
+}
